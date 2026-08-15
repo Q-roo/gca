a66e70c6c5746de72dab7cc9720caae46877979a
Q-roo <kovacs113istvan@gmail.com>
2026-08-15 14:37:37 -0200

    clarify config values in gc-config

--- include/gca/gc-config.h
+++ include/gca/gc-config.h
@@ -9,9 +9,9 @@ namespace gc::config {
     using object_flags_underlying_type = std::uint16_t;
     using object_root_handle_count_underlying_type = std::uint16_t;
     using gc_impl_ongoing_gc_count_underlying_type = std::uint8_t;
-    constexpr bool gc_throw_nonessential_exceptions = true; // AKA exceptions instead of debug assertions
+    constexpr bool gc_throw_nonessential_exceptions = true; // mostly for out-of-bound indices in gc-util.h
     constexpr bool enable_debug_messages = true;
-    constexpr bool enable_assertions = true;
+    constexpr bool enable_assertions = true; // for bugs within gc_impl
     constexpr std::size_t page_size = 4096;
     constexpr std::size_t page_alignment = 4096;
     constexpr std::size_t page_acquire_attempts = 10;
